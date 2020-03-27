18250d7c559f36e980e933080e3c0d8629e8b3fa
Asa Zernik <asa.zernik@gmail.com>
2020-03-27 15:45:54 -0700

    SlickFutureSystem: Parameterize DB type

--- src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
+++ src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
@@ -1,12 +1,14 @@
 package com.parsleycooks.slickAsync
 
+import slick.basic.{BasicProfile, DatabaseConfig}
+
 import scala.async.internal.FutureSystem
 import scala.reflect.macros.whitebox
 
-class SlickFutureSystem extends FutureSystem {
+class SlickFutureSystem[P <: BasicProfile] extends FutureSystem {
   override type Prom = this.type
   override type Fut = this.type
-  override type ExecContext = this.type
+  override type ExecContext = DatabaseConfig[P] // TODO: maybe use an ActionContext? need to figure out
   override type Tryy = this.type
 
   override def mkOps(c0: whitebox.Context): Ops {
