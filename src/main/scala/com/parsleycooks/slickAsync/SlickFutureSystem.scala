be959d30f3c1e6b736c8186151a604b6a7bcd76a
Asa Zernik <asa.zernik@gmail.com>
2020-03-27 16:13:39 -0700

    SlickFutureSystem: Add type parameters for result types

--- src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
+++ src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
@@ -6,10 +6,10 @@ import scala.async.internal.FutureSystem
 import scala.reflect.macros.whitebox
 
 class SlickFutureSystem[P <: BasicProfile] extends FutureSystem {
-  override type Prom = this.type
-  override type Fut = this.type
+  override type Prom[A] = this.type
+  override type Fut[A] = this.type // TODO: probably DBIO
   override type ExecContext = DatabaseConfig[P] // TODO: maybe use an ActionContext? need to figure out
-  override type Tryy = this.type
+  override type Tryy[A] = scala.util.Try[A]
 
   override def mkOps(c0: whitebox.Context): Ops {
     val c: c0.type
