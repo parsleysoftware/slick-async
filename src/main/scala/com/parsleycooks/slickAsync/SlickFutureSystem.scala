230ac8d12564e1e41cf42830cfe90bb6a9920658
Asa Zernik <asa.zernik@gmail.com>
2020-03-27 15:43:49 -0700

    Add Ops boilerplate to SlickFutureSystem
    Mostly copied from ScalaConcurrentFutureSystem, but with explicit
    override declarations so I'll get "doesn't override anything" warnings
    if I mess up a type declaration.

--- src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
+++ src/main/scala/com/parsleycooks/slickAsync/SlickFutureSystem.scala
@@ -11,7 +11,48 @@ class SlickFutureSystem[P <: BasicProfile] extends FutureSystem {
   override type ExecContext = DatabaseConfig[P] // TODO: maybe use an ActionContext? need to figure out
   override type Tryy[A] = scala.util.Try[A]
 
-  override def mkOps(c0: whitebox.Context): Ops {
-    val c: c0.type
-  } = ???
+  override def mkOps(c0: whitebox.Context): Ops {val c: c0.type} = new Ops {
+    val c: c0.type = c0
+    import c.universe._
+
+    override def promType[A: WeakTypeTag] = weakTypeOf[Prom[A]] // TODO sub in
+    override def tryType[A: WeakTypeTag]: Type = weakTypeOf[scala.util.Try[A]]
+    override def execContextType: Type = weakTypeOf[DatabaseConfig[P]]
+
+    override def createProm[A: WeakTypeTag]: Expr[Prom[A]] = reify {
+      ???
+    }
+
+    override def promiseToFuture[A: WeakTypeTag](prom: Expr[Prom[A]]): Expr[Fut[A]] = reify {
+      ???
+    }
+
+    override def future[A: WeakTypeTag](a: Expr[A])(execContext: Expr[ExecContext]): Expr[Fut[A]] = reify {
+      ???
+    }
+
+    override def onComplete[A, U](future: Expr[Fut[A]], fun: Expr[Tryy[A] => U], execContext: Expr[ExecContext]): Expr[Unit] = reify {
+      ???
+    }
+
+    override def completeProm[A](prom: Expr[Prom[A]], value: Expr[Tryy[A]]): Expr[Unit] = reify {
+      ???
+    }
+
+    override def tryyIsFailure[A](tryy: Expr[Tryy[A]]): Expr[Boolean] = reify {
+      ???
+    }
+
+    override def tryyGet[A](tryy: Expr[Tryy[A]]): Expr[A] = reify {
+      ???
+    }
+
+    override def tryySuccess[A: WeakTypeTag](a: Expr[A]): Expr[Tryy[A]] = reify {
+      ???
+    }
+
+    override def tryyFailure[A: WeakTypeTag](a: Expr[Throwable]): Expr[Tryy[A]] = reify {
+      ???
+    }
+  }
 }
